4c0dafcad48db7f4e1b160d469a88a61587ce79b
etherusko <etherusko.labs@gmail.com>
2024-05-20 13:41:25 -0600

    refactor: display flex

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d',{alpha: false});
-canvas.width = 500;
-canvas.height = 500;
+//canvas.width = 500;
+//canvas.height = 500;
 
 ctx.fillStyle = 'orange';
-ctx.fillRect(0,0,25,25);
+ctx.fillRect(0,0,30,30);
