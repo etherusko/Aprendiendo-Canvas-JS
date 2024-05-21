0ba4940073f4b272682aa75bf9ddc6443fd82628
etherusko <etherusko.labs@gmail.com>
2024-05-21 15:36:55 -0600

    minor changes

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
-const canvas = document.getElementById('canvas');
+
+const canvas = document.getElementById('canvas')
+
 const ctx = canvas.getContext('2d',{alpha: false});
 const cw = 500;
 const ch = 500;
 canvas.width = cw;
 canvas.height = ch;
-ctx.fillStyle = 'orange';
-//ctx.fillRect(0,0,30,30);
-//  dibujarRectangulo(50,50);
+
+const btnRect = document.getElementById("B-rectangulo");
+const btnClean = document.getElementById("B-borrar");
+btnRect.addEventListener("click",() => dibujarRectangulo(50,50));
+btnClean.addEventListener("click",limpiarCanvas);
+
+ctx.strokeStyle = 'orange';
+ctx.strokeRect(0,0,25,52);
+
 function dibujarRectangulo(x,y){
-    ctx.fillRect(cw/2-x,ch/2-y,x,y);
+    ctx.fillStyle = 'orange';
+    ctx.fillRect((cw-x)/2,(ch-y)/2,x,y);
+}
+function limpiarCanvas(){
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0,0,cw,ch);
 }
