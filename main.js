b382544a489a3301172fa45138d0d39d0d04c384
etherusko <etherusko.labs@gmail.com>
2024-05-21 03:21:12 -0600

    Se agregó contenido (Basics de Canvas JS)

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-const canvas = document.querySelector('canvas');
+const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d',{alpha: false});
-//canvas.width = 500;
-//canvas.height = 500;
-
+const cw = 500;
+const ch = 500;
+canvas.width = cw;
+canvas.height = ch;
 ctx.fillStyle = 'orange';
-ctx.fillRect(0,0,30,30);
+//ctx.fillRect(0,0,30,30);
+//  dibujarRectangulo(50,50);
+function dibujarRectangulo(x,y){
+    ctx.fillRect(cw/2-x,ch/2-y,x,y);
+}
